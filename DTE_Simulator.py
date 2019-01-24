a66ce0c2035552a854acf801966113fad7962400
ChristianHokayem <christian.hokayem@ieee.org>
2019-01-24 22:20:18 +0200

    changed from time to packets

--- DTE_Simulator.py
+++ DTE_Simulator.py
@@ -1,42 +1,44 @@
 from random import expovariate
 from time import time
+import argparse
+
+parent_parser = argparse.ArgumentParser(add_help=False)
+parent_parser.add_argument('LAMBDA', type=float)
+args = parent_parser.parse_args()
 
 START_TIME = time()
+LAMBDA = args.LAMBDA
+print("\n"*2)
+print("-"*10)
+print("LAMBDA:", LAMBDA)
 
 TIME_ADVANCE = 1e-6 #in time unit
-MAX_REAL_TIME = 6000 #in time unit
-AVG_INTERARRIVAL_TIME = 1/97 #in time unit
+AVG_INTERARRIVAL_TIME = 1/LAMBDA #in time unit
 AVG_SERVICE_TIME = 1/250 #in time unit
-
-MAX_SIM_TIME = int(MAX_REAL_TIME//TIME_ADVANCE)
+PACKETS_TARGET = 100000 #number of pcakets to simulate
 
 def discrete_expovariate_time(mean):
     global TIME_ADVANCE
     return round(expovariate(1/mean)/TIME_ADVANCE)
 
-def generate_discrete_future_packet_times(start_time = 0, end_time = MAX_SIM_TIME, avg_interarrival_time = AVG_INTERARRIVAL_TIME):
+def generate_discrete_future_packet_arrival_times(start_time = 0, avg_interarrival_time = AVG_INTERARRIVAL_TIME):
     arrival_times = []
 
     packet_arrival = start_time + discrete_expovariate_time(AVG_INTERARRIVAL_TIME)
-    while packet_arrival < MAX_SIM_TIME:
-
+    for _ in range(PACKETS_TARGET):
         arrival_times.append(packet_arrival)
         packet_arrival += discrete_expovariate_time(AVG_INTERARRIVAL_TIME)
 
     return arrival_times
 
 
-future_events = generate_discrete_future_packet_times()
+future_events = generate_discrete_future_packet_arrival_times()
 master_clock = 0
-
 wait_times = []
 serviced_times = []
 busy = False
 
-while master_clock <= MAX_SIM_TIME:
-    if len(future_events) == 0:
-        break
-
+while len(future_events) > 0 or busy == True:
     if not(busy):
         if future_events[0] <= master_clock:
             next_service = master_clock + discrete_expovariate_time(AVG_SERVICE_TIME)
@@ -57,13 +59,10 @@ while master_clock <= MAX_SIM_TIME:
 
     master_clock += 1
 
-wait_time_string = "AVERAGE TOTAL WAIT TIME: " + str((sum(wait_times)/len(wait_times))*TIME_ADVANCE)
-print("-"*len(wait_time_string))
-print(wait_time_string)
-print("-"*len(wait_time_string))
-print("PACKETS WAITING:", len(future_events))
-print("PACKETS SERVICED:", len(wait_times))
+print("AVERAGE TOTAL WAIT TIME: " + str((sum(wait_times)/len(wait_times))*TIME_ADVANCE))
 
 END_TIME = time()
 
 print("SCRIPT TIME:", END_TIME - START_TIME)
+
+print("-"*10)
