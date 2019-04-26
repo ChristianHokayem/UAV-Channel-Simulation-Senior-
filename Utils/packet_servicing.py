88bb5b09be95990310a11c68bb865c2be4d2be94
ChristianHokayem <christian.hokayem@ieee.org>
2019-04-27 02:39:32 +0300

    MANY FIXES

--- Utils/packet_servicing.py
+++ Utils/packet_servicing.py
@@ -34,16 +34,15 @@ def generate_packet_service_event(master_clock, popped_packet):
 
 
 def compute_rayleigh_capacity(pr_avg, allocated_resource_blocks):
-  sigma = sqrt(pr_avg)
-  faded_power = rayleigh.rvs(scale=sigma)
+  sigma = sqrt(pr_avg / 2)
+  faded_power = rayleigh.rvs(scale=sigma) ** 2
   rb_capacity = allocated_resource_blocks * RB_BANDWIDTH * log2(1 + faded_power / RB_NOISE)
   return rb_capacity
 
 
 def compute_rice_capacity(pr_avg, allocated_resource_blocks):
-  v = sqrt(K * 2) * sqrt(pr_avg)
-  scale_param = v ** 2 + 2 * sqrt(pr_avg) ** 2
-  faded_power = rice.rvs(b=K, scale=scale_param)
+  scale_param = sqrt(pr_avg / (2 * (K + 1)))
+  faded_power = rice.rvs(b=K, scale=scale_param) ** 2
   rb_capacity = allocated_resource_blocks * RB_BANDWIDTH * log2(1 + faded_power / RB_NOISE)
   return rb_capacity
 
@@ -54,9 +53,9 @@ def compute_a2g_received_power(transmit_power_db, distance, height):
 
 
 def compute_g2g_received_power(transmit_power_db, distance):
-  los_probability = g2g_los_probability(distance)
-  received_power_db = transmit_power_db - ((103.4 + 24.2 * log10(distance)) * los_probability
-                                           + (131.1 + 42.8 * log10(distance)) * (1 - los_probability))
+  los_probability = g2g_los_probability(distance * 0.001)
+  received_power_db = transmit_power_db - ((103.4 + 24.2 * log10(distance * 0.001)) * los_probability
+                                           + (131.1 + 42.8 * log10(distance * 0.001)) * (1 - los_probability))
   return 10 ** (received_power_db / 10)
 
 
